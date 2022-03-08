c35c6a57505883bbf6506576a4b76fd2e6f6e2d3
Casper Jensen <casperbojensen@gmail.com>
2022-03-08 10:08:40 +0100

    app update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.write('Hello world')
 
 st.write('Perens atlas')
 
 st.write('Git ny test')
+
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
 
 
