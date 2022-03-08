96d9df3e14b5f4899b726b1da6371c94a7fec152
Casper Jensen <casperbojensen@gmail.com>
2022-03-08 14:32:07 +0100

    app update

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,9 +72,9 @@ with col1:
         st.image(mri[:,pos,:])
     if option_orientation=='Sagital':
         pos = st.slider('Atlas positions', min_value=0, max_value=mri.shape[2], value=int(mri.shape[2] / 2), step=None)
-        image = Image.open('horizontal_white_neuropedia/'+df_highligt+'.tif')
+        image = Image.open('horizontal_white_neuropedia/'+option_highligt+'.tif')
         st.image(image)
-        st.image('horizontal_white_neuropedia/' + df_highligt + '.tif')
+        st.image('horizontal_white_neuropedia/' + option_highligt + '.tif')
         st.image(mri[:, :, pos])
 with col2:
     if option_orientation == 'Coronal':
