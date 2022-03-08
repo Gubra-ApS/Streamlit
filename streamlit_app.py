eeafb0d3b92f5ce813983039c8cd97f385ff23b1
Casper Jensen <casperbojensen@gmail.com>
2022-03-08 10:10:21 +0100

    app update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.write('Hello world')
 
@@ -11,5 +12,8 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
 
 
