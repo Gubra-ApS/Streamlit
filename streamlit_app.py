9850d89b71dff1eef01e599ae514b29454244352
Casper Jensen <casperbojensen@gmail.com>
2022-03-08 15:17:01 +0100

    app update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ import time
 import SimpleITK as sitk
 from PIL import Image
 
+# create session variables
+if 'y_val' not in st.session_state:
+    st.session_state['y_val'] = '300'
+
 @st.cache  # 👈 This function will be cached
 def read_atlases(read):
     # load atlas files only once
@@ -53,6 +57,10 @@ st.sidebar.button('Go to region centre')
 # 'You selected orientation: ', option_orientation
 # 'You selected regions: ', option_orientation
 
+st.sidebar.header('Coordinate finder')
+x = st.sidebar.text_input('x (medial-laterally):', '0')
+y = st.sidebar.text_input('y (anterior-posterior):', st.session_state.x_val)
+z = st.sidebar.text_input('z (dorsal-ventral):', '0')
 
 #
 st.header('Stereotxic coordinate [' + x + ', ' + y + ', ' + z + ']')
@@ -61,30 +69,26 @@ col1, col2 = st.columns(2)
 with col1:
     if option_orientation=='Coronal':
         if st.button('Next'):
-            y = str(int(y)+10)
+            st.session_state.y_val = str(int(st.session_state.y_val)+10)
         #st.sidebar.button('Go to region centre')
 
         image = Image.open('horizontal_white_neuropedia/'+option_highligt+'.tif')
         st.image(image)
-        st.image(mri[:,int(y),:])
+        st.image(mri[:,int(st.session_state.y_val),:])
     if option_orientation=='Sagital':
         image = Image.open('horizontal_white_neuropedia/'+option_highligt+'.tif')
         st.image(image)
         st.image(mri[:, :, int(x)])
 with col2:
     if option_orientation == 'Coronal':
-        st.image(lsfm[:,int(y),:])
-        st.image(ccfv3[:,int(y),:])
+        st.image(lsfm[:,int(st.session_state.y_val),:])
+        st.image(ccfv3[:,int(st.session_state.y_val),:])
     if option_orientation == 'Sagital':
         st.image(lsfm[:, :, int(x)])
         st.image(ccfv3[:, :, int(x)])
 
 
 
-st.sidebar.header('Coordinate finder')
-x = st.sidebar.text_input('x (medial-laterally):', '0')
-y = st.sidebar.text_input('y (anterior-posterior):', '300')
-z = st.sidebar.text_input('z (dorsal-ventral):', '0')
 
 
 
