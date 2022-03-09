22c8180502c7486db36720a885cdacdbe9debf55
Casper Jensen <casperbojensen@gmail.com>
2022-03-09 11:42:41 +0100

    Atlas app

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,7 +109,7 @@ st.write('horizontal_white_neuropedia/'+option_highligt+'.tif')
 
 # Create a canvas component
 canvas_result = st_canvas(
-    stroke_width=3,
+    stroke_width=0,
     stroke_color="black",
     background_image=im_plot(mri[:,int(st.session_state.y_val),:]),
     height=297,
