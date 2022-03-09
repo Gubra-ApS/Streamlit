bb390c981033d347f5828830a9f2c1c91199b08d
Casper Jensen <casperbojensen@gmail.com>
2022-03-09 11:18:32 +0100

    Atlas app

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def fig2img(fig):
 def im_plot(im):
     px = 1 / plt.rcParams['figure.dpi']
     # fig = plt.figure(figsize=(int(455*px), int(297*px)))
-    fig = plt.figure(figsize=(15.3, 10))
+    fig = plt.figure(figsize=(15.319, 10))
     plt.imshow(im, cmap='gray')
     plt.axis('off')
     plt.tight_layout()
