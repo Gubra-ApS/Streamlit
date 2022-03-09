70673e2f1432449f966828efaf8d9b8d24f32642
Casper Jensen <casperbojensen@gmail.com>
2022-03-09 08:51:20 +0100

    Atlas app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 import SimpleITK as sitk
 from PIL import Image
+from streamlit-drawable-canvas import st_canvas
 
 # create session variables
 if 'y_val' not in st.session_state:
@@ -68,30 +69,85 @@ if st.sidebar.button('Got o coordinate'):
 #
 st.header('Stereotxic coordinate [' + x + ', ' + st.session_state.y_val + ', ' + z + ']')
 
-col1, col2 = st.columns(2)
-with col1:
-    if option_orientation=='Coronal':
-        if st.button('Next'):
-            st.session_state.y_val = str(int(st.session_state.y_val)+10)
-        if st.button('Prev'):
-            st.session_state.y_val = str(int(st.session_state.y_val)-10)
-        #st.sidebar.button('Go to region centre')
 
-        image = Image.open('horizontal_white_neuropedia/'+option_highligt+'.tif')
-        st.image(image)
-        st.image(mri[:,int(st.session_state.y_val),:])
-    if option_orientation=='Sagital':
-        image = Image.open('horizontal_white_neuropedia/'+option_highligt+'.tif')
-        st.image(image)
-        st.image(mri[:, :, int(x)])
-with col2:
-    if option_orientation == 'Coronal':
-        st.image(lsfm[:,int(st.session_state.y_val),:])
-        st.image(ccfv3[:,int(st.session_state.y_val),:])
-    if option_orientation == 'Sagital':
-        st.image(lsfm[:, :, int(x)])
-        st.image(ccfv3[:, :, int(x)])
 
+
+
+# Specify canvas parameters in application
+drawing_mode = st.sidebar.selectbox(
+    "Drawing tool:",
+    ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+)
+stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+if drawing_mode == 'point':
+    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+realtime_update = st.sidebar.checkbox("Update in realtime", True)
+
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    background_image=Image.open(bg_image) if bg_image else None,
+    update_streamlit=realtime_update,
+    height=150,
+    drawing_mode=drawing_mode,
+    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+    display_toolbar=st.sidebar.checkbox("Display toolbar", True),
+    key="full_app",
+)
+
+# Do something interesting with the image data and paths
+if canvas_result.image_data is not None:
+    st.image(canvas_result.image_data)
+if canvas_result.json_data is not None:
+    objects = pd.json_normalize(canvas_result.json_data["objects"])
+    for col in objects.select_dtypes(include=["object"]).columns:
+        objects[col] = objects[col].astype("str")
+    st.dataframe(objects)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+# col1, col2 = st.columns(2)
+# with col1:
+#     if option_orientation=='Coronal':
+#         if st.button('Next'):
+#             st.session_state.y_val = str(int(st.session_state.y_val)+10)
+#         if st.button('Prev'):
+#             st.session_state.y_val = str(int(st.session_state.y_val)-10)
+#         #st.sidebar.button('Go to region centre')
+#
+#         image = Image.open('horizontal_white_neuropedia/'+option_highligt+'.tif')
+#         st.image(image)
+#         st.image(mri[:,int(st.session_state.y_val),:])
+#     if option_orientation=='Sagital':
+#         image = Image.open('horizontal_white_neuropedia/'+option_highligt+'.tif')
+#         st.image(image)
+#         st.image(mri[:, :, int(x)])
+# with col2:
+#     if option_orientation == 'Coronal':
+#         st.image(lsfm[:,int(st.session_state.y_val),:])
+#         st.image(ccfv3[:,int(st.session_state.y_val),:])
+#     if option_orientation == 'Sagital':
+#         st.image(lsfm[:, :, int(x)])
+#         st.image(ccfv3[:, :, int(x)])
+#
 
 
 
