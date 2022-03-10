0ece35099f4277d0d8d50351d7817eefe4ffd2ae
Casper Jensen <casperbojensen@gmail.com>
2022-03-10 20:55:32 +0100

    app update

--- streamlit_app.py
+++ streamlit_app.py
@@ -159,6 +159,8 @@ with col1:
         stroke_width=0,
         stroke_color="black",
         background_image=im_click,
+        height=im_click_pre.shape[0],
+        width=im_click_pre.shape[1],
         drawing_mode="circle",
         display_toolbar=False,
         key="center_circle_app"
