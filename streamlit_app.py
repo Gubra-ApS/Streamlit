2bd41b3eaecb0274fb06b3e4725f541c950c48e4
Casper Jensen <casperbojensen@gmail.com>
2022-03-11 13:05:42 +0100

    app update

--- streamlit_app.py
+++ streamlit_app.py
@@ -179,7 +179,7 @@ with st.container():
         image = Image.open('horizontal_white_neuropedia/' + option_highligt + '.tif')
         pix = np.array(image)
         pix = np.swapaxes(pix, 0, 1)
-        y_coord = int(int(float(st.session_state.y_val)) / 512 * 199) + 20
+        y_coord = int(int(float(st.session_state.y_val)) / 512 * 199)
         im_mip = helpers.im_plot_mip(pix, y_coord)
         st.image(im_mip, width=340)
 
